a8bc3ac23d2a6024fe73eec2d836bdd91ed5ae59
PrajaktaShewale <shewalep94@gmail.com>
2022-12-21 17:19:39 +0530

    [Prajakta]Add:Valid first Name

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;//namespace for regex
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace UserRegistration
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidUserInfo re = new ValidUserInfo();
+            Console.WriteLine("Enter first Name");
+            string first = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(re.validateFirstName(first));
+        }        
+    }
+}
---
+++ UserRegistration/ValidUserInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UserRegistration
+{
+    internal class ValidUserInfo
+    {
+        public static string FIRSTNAME = "^[A-Z]{1}[a-z]{2,}";
+
+        public bool validateFirstName(string fname)
+        {
+            return Regex.IsMatch(fname, FIRSTNAME);
+        }
+    }
+}
